cd17dbd3b61c6b3352d8db68246a996af0a240af
noordinq <nooshqureish@gmail.com>
2024-05-15 20:33:40 +0300

    main class with methods

--- script.js
+++ script.js
@@ -100,3 +100,77 @@ products.forEach(
       `;
     }
 );
+
+class ShoppingCart {
+    constructor() {
+      this.items = [];
+      this.total = 0;
+      this.taxRate = 8.25;
+    }
+  
+    addItem(id, products) {
+      const product = products.find((item) => item.id === id);
+      const { name, price } = product;
+      this.items.push(product);
+  
+      const totalCountPerProduct = {};
+      this.items.forEach((dessert) => {
+        totalCountPerProduct[dessert.id] = (totalCountPerProduct[dessert.id] || 0) + 1;
+      })
+  
+      const currentProductCount = totalCountPerProduct[product.id];
+      const currentProductCountSpan = document.getElementById(`product-count-for-id${id}`);
+  
+      currentProductCount > 1 
+        ? currentProductCountSpan.textContent = `${currentProductCount}x`
+        : productsContainer.innerHTML += `
+        <div id="dessert${id}" class="product">
+          <p>
+            <span class="product-count" id="product-count-for-id${id}"></span>${name}
+          </p>
+          <p>${price}</p>
+        </div>
+        `;
+    }
+  
+    getCounts() {
+      return this.items.length;
+    }
+  
+    clearCart() {
+      if (!this.items.length) {
+        alert("Your shopping cart is already empty");
+        return;
+      }
+
+      const isCartCleared = confirm(
+        "Are you sure you want to clear all items from your shopping cart?"
+      );
+  
+      if (isCartCleared) {
+        this.items = [];
+        this.total = 0;
+        productsContainer.innerHTML = "";
+        totalNumberOfItems.textContent = 0;
+        cartSubTotal.textContent = 0;
+        cartTaxes.textContent = 0;
+        cartTotal.textContent = 0;
+      }
+    }
+  
+    calculateTaxes(amount) {
+      return parseFloat(((this.taxRate / 100) * amount).toFixed(2));
+    }
+  
+    calculateTotal() {
+      const subTotal = this.items.reduce((total, item) => total + item.price, 0);
+      const tax = this.calculateTaxes(subTotal);
+      this.total = subTotal + tax;
+      cartSubTotal.textContent = `$${subTotal.toFixed(2)}`;
+      cartTaxes.textContent = `$${tax.toFixed(2)}`;
+      cartTotal.textContent = `$${this.total.toFixed(2)}`;
+      return this.total;
+    }
+}
+
+;
